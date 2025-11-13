5f266d763ce6ac49cfd216a95cc7470f211a19d6
Nicolas <nimorenoca@unal.edu.co>
2025-11-12 20:36:39 -0500

    Ya las principales funcionalidades de inicio de sesión implementadas

--- Db.js
+++ Db.js
@@ -3,6 +3,34 @@ import {
   getFirestore,
   collection,
   addDoc,
+  query,
+  where,
   getDocs,
 } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 const db = getFirestore(app);
+export async function RegisterAccount(username, email,uid){
+  try{
+    await addDoc(collection(db,"Usuarios"),
+    {
+      username: username,
+      email: email,
+      UID: uid
+    })
+  }
+  catch(error){
+    console.error("error",error);
+    throw error;
+    
+  }
+}
+export async function CheckEmail(email){
+  try{
+    const q = query(collection(db,"Usuarios"), where("email", "==", email));
+    const queryState = await getDocs(q);
+    return queryState
+  }
+  catch(error){
+    console.error("error",error);
+    throw error;
+  }
+}
--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 // script.js (módulo de UI, importa login/logout desde Auth.js)
-import { login, logout, auth } from "./Auth.js";
+import { loginGoogle, logout, auth, CreateEmailAndPassword, LoginEmailAndPassword } from "./Auth.js";
+import { RegisterAccount, CheckEmail } from "./Db.js";
 
 const buttonLogin = document.querySelector("#button-login");
-const buttonLogout = document.querySelector("#button-logout");
+const buttonLogout = document.querySelector("#button-logout2");
 const inputDemo = document.querySelector("#input-demo");
 const formulario = document.querySelector(".formulario");
 const fuffy = document.querySelector("#fuffy");
 const buttonGoogle = document.querySelector("#button-google");
 const buttonRegister = document.querySelector("#button-register");
 const RegisterPopup = document.querySelector("#RegistroPopup");
+const buttonSendRegister = document.querySelector("#Send-Register");
 const buttonCancRegister = document.querySelector("#Cancel-Register");
 
 // Evitamos que el click haga submit o recargue
 buttonGoogle.addEventListener("click", async (e) => {
   e.preventDefault();
   try {
-    const user = await login();
+    const user = await loginGoogle();
+    let email = String(user.email);
+    console.log(email);
+    let username = String(user.displayName);
+    console.log(email);
+    let UID = String(user.uid);
+    console.log(email);
     // Puedes mostrar datos del usuario en pantalla
     if (user) {
       console.log("Usuario logueado:", user.displayName, user.email);
       // ejemplo: rellenar el input con el email
       inputDemo.value = user.email ?? "";
+      let result = await CheckEmail(user.email);
+      if (result.empty){
+        await RegisterAccount(username, email,UID);
+      }
+
       init();
     }
 
@@ -37,27 +50,24 @@ buttonLogout.addEventListener("click", async (e) => {
     await logout();
     inputDemo.value = "";
     console.log("Sesión cerrada");
+    reset();
   } catch (err) {
     console.error("Fallo logout:", err);
     alert("Error al cerrar sesión. Revisa la consola.");
   }
 });
 
-// Opcional: escucha cambios de estado de auth
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    console.log("onAuthStateChanged: user signed in:", user.email);
-  } else {
-    console.log("onAuthStateChanged: no user");
-  }
-});
-// funcion para ocultar todo lo necesario cuando el login se completa
+
+// funciones para ocultar todo lo necesario cuando el login se completa, y para volver a mostrarlo al cerrar sesion 
 function init(){
     if(auth.currentUser){
         formulario.classList.add("hidden");
         fuffy.classList.remove("hidden");
     }
+}
+function reset(){
+    formulario.classList.remove("hidden");
+    fuffy.classList.add("hidden");
 }
 // funcion para hacer aparecer el popup de registro
 buttonRegister.addEventListener("click", (e) => {
@@ -72,3 +82,52 @@ buttonCancRegister.addEventListener("click", (e) => {
   RegisterPopup.classList.remove("RegistroPopup");
   RegisterPopup.close();
 })
+// funcion para enviar el los datos de registro a la base de datos
+buttonSendRegister.addEventListener("click", async (e) => {
+  e.preventDefault();
+  try{
+      let username = document.getElementById("Input-Username").value;
+      let email = document.getElementById("Input-Email").value;
+      let password = document.getElementById("Input-Password").value;
+  if(username == "" || email == "" || password == "" ){
+    return
+  }
+  let result = await CreateEmailAndPassword(email, password);
+  let user = result.user;
+  let uid = user.uid;
+  await RegisterAccount(username, email, uid);
+  RegisterPopup.close();
+  console.log("Cuenta creada");
+  }
+  catch(error){
+    console.error("error", error);
+    throw error;
+  }
+})
+// Funcion para iniciar sesión con correo y contraseña propios
+buttonLogin.addEventListener("click", async (e) => 
+{
+  e.preventDefault();
+  try {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if(email == "" || password == "" ){
+      return
+    }
+    let ThereIsEmail = await CheckEmail(email);
+    if(ThereIsEmail.empty){
+      console.log("El correo no existe");
+      return
+    }
+    let result = await LoginEmailAndPassword(email, password);
+    let user = result.user;
+    console.log("Login correcto:", user);
+    init()
+    }
+  catch(error){
+    console.error("error",error);
+    throw error;
+  }
+}
+)
+
