f40895bd426190d7dc08e4813b448ca7f8920de6
Richi :D <144559210+Skylethx@users.noreply.github.com>
2025-11-18 21:43:25 -0500

    Update diaryRoutes.js

--- src/routes/diaryRoutes.js
+++ src/routes/diaryRoutes.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { verifyAuth } = require("./auth"); 
-const diaryController = require("../controllers/diaryController");
+const verifyAuth = require('../middleware/verifyAuth');
+const diaryController = require('../controllers/diaryController');
 
-router.post("/", verifyAuth, diaryController.createEntry);
-router.get("/", verifyAuth, diaryController.getEntries);
-router.get("/:id", verifyAuth, diaryController.getEntryById);
-router.put("/:id", verifyAuth, diaryController.updateEntry);
-router.delete("/:id", verifyAuth, diaryController.deleteEntry);
+// CRUD básico
+router.post('/', verifyAuth, diaryController.createEntry);
+router.get('/', verifyAuth, diaryController.getEntries);
+router.get('/:id', verifyAuth, diaryController.getEntryById);
+router.put('/:id', verifyAuth, diaryController.updateEntry);
+router.delete('/:id', verifyAuth, diaryController.deleteEntry);
 
 module.exports = router;
